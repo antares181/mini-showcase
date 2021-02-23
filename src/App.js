a95003166d4bc2e614ad077fa529dcfaab4eeda2
harum.shidiqi <harum.shidiqi@mavila.co.id>
2021-02-23 22:57:19 +0700

    new sketchup model

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <iframe title="showcase" src="https://3dwarehouse.sketchup.com/embed/7f27c179-9abb-48b0-9247-e676df1c1ee3" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" width="100%" height="100%" allowFullScreen></iframe> */}
+      <iframe title="productivity-table" src="https://embed-3dwarehouse.sketchup.com/embed/d4fd2534-c68c-4119-9bcc-17cc69d5ef70" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" width="100%" height="100%" allowfullscreen></iframe>
+
     </div>
   );
 }
